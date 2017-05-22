95cf90da1fbf59f93d7ef44eb0f223c403df627c
CWDavis <Chadjdavis21@gmail.com>
2017-05-22 13:21:18 -0700

    Updated Robber class
    Added exclusion to randomly picking a resource, checkHex and a pickPlayer method. Picking a specific hex and a player from a list of players still needs implementing

--- SoM/src/som/Robber.java
+++ SoM/src/som/Robber.java
@@ -22,55 +22,114 @@ public class Robber {
     public void AcitaveRobber(Player player)
     {
         CurrentPlayer = player;
-        SevenResourceCheck();
-        MoveRobber();        
+        SevenResourceCheck();               
     }
     
+    /*
+     * This method checks each player if they have 7 or more resources and has them randomly
+     * remove half of their resources (rounding down)
+     */
     private void SevenResourceCheck()
     {
-        
-        for(int i = 0; i < players.size() -1; i++) //for loop runs once for every player
+        int randomResourceNumber = 0;
+        for(int i = 0; i < players.size(); i++) //for loop runs once for every player
         {
             if(players.get(i).countResources() >= 7) //If the current player has more than 7 resource cards...
             {
-                for(int j = 0; j < (players.get(i).countResources()/2) -1; j++) //for loop to run half of number of player resource cards, rounding down. Ex: player has 7 cards it will run 3 times
+                for(int j = 0; j < (players.get(i).countResources()/2); j++) //for loop to run half of number of player resource cards, rounding down. Ex: player has 7 cards it will run 3 times
                 {
-                    players.get(i).resources[RandomResourceValue(i)]--;    //randomly removes one resource dictated by RandomResourceValue method  
-                    resourceBank.resourceList.get(i).returnResource(i);
+                    randomResourceNumber = RandomResourceValue(i);
+                    players.get(i).resources[randomResourceNumber]--;    //randomly removes one resource dictated by RandomResourceValue method  
+                    resourceBank.bankReturnResource(randomResourceNumber,1);
                 } //end of inner for loop
             } //end of inner if statment
         } //end of for loop
         
-        MoveRobber();
+        MoveRobber(CurrentPlayer);
     }
     
-    private int RandomResourceValue(int playerNumber) //Need to set exceptions to random so prevent endless recurrision
+    /*
+     * This randomly generates a number bewteen 0 and 4 and checks if the
+     * corresponding player has said resourceNumber in their resources[]
+     */
+    private int RandomResourceValue(int playerNumber)
     {
+        
+        ArrayList<Integer> excludedNumbers = new ArrayList<>();
+                
+         for(int i = 0; i < 5; i++)
+        {
+            if(players.get(playerNumber).resources[i] == 0)
+                excludedNumbers.add(i);
+        }
+         
         Random r = new Random();
         int randomVariable = 0;
         randomVariable = r.nextInt(5-0)+0; // IF THERE IS AN INDEX ERROR:THIS MIGHT BE THE CAUSE: This should return a number between 0-4
         
-        if(players.get(playerNumber).resources[randomVariable] == 0) //If the player does not have the resource matching the random number: the program runs again
-        {
-            RandomResourceValue(playerNumber);
+        
+        while(excludedNumbers.contains(randomVariable)){ //While loop to run until the random number generated is not in the excludedNumber arrayList
+            randomVariable = r.nextInt(5-0)+0; // IF THERE IS AN INDEX ERROR:THIS MIGHT BE THE CAUSE: This should return a number between 0-4
         }
+        
         return randomVariable;
     }
-       
-    private void MoveRobber()
+    
+    /*
+     * This method allows the current player to move the robber to a desired hex
+     */
+    public void MoveRobber(Player player)
     {
-        //Needs Implementation
-        StealResources(pickedPlayer);
+        //Needs Implementation to move Robber to specific hex
+        Hex pickedHex = new Hex();
+        //pickedHex = the specific hex
+        //pickedHex can no longer generate resources until rober is moved again
+        
+        checkHex(pickedHex);
     }
     
-    private void StealResources(Player player)
+    /*
+     * This method checks the specific hex for players
+     * Made public for the knight card
+     */
+    private void checkHex(Hex pickedHex)
     {
-     //Needs Implementation
+     ArrayList<Player>listOfplayers= new ArrayList<>();
+	
+        for(int eachVertex=0;eachVertex<6;eachVertex++){
+             if(pickedHex.getVerticies().get(eachVertex).getAsset()!=null)
+                 {
+                   listOfplayers.add(pickedHex.getVerticies().get(eachVertex).getAsset().getPlayer());
+                   }
+         }
+        
+        pickPlayer(listOfplayers);
+        
     }
     
-    public void driver()
-    {
-       //Needs Implementation  
+    /*
+     * This method will either remove a random resources from one player and give it to the current player
+     * or will allow the player to pick from a list of players on a specific hex
+     * and remove a random resource from the picked player and give it to the current player
+     */
+    private void pickPlayer(ArrayList<Player> listOfplayers)
+    {   
+        int randomResourceNumber = 0;
+        randomResourceNumber = RandomResourceValue(0);
+        
+        if(listOfplayers.size() == 1)
+        {
+            listOfplayers.get(0).resources[randomResourceNumber]--;
+            CurrentPlayer.resources[randomResourceNumber]++;
+        }
+        else 
+        {
+            //pick player from list of players
+            //pickedPlayer = listOfplayers[?]
+            //pickedPlayer.resources[randomResourceNumber]--;
+            //CurrentPlayer.resources[randomResourceNumber]++;
+        }
+        
     }
-    
+        
 }
