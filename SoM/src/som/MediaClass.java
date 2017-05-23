b5ff1944b9b9164c7ddbeb2386f4f832fc17b9b4
dano80 <danielino3@yahoo.com>
2017-05-23 11:14:40 -0700

    Added audio and graphics functionality

---
+++ SoM/src/som/MediaClass.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package som;
+
+import com.sun.javafx.iio.ImageLoader;
+import java.io.InputStream;
+import javafx.scene.image.Image;
+
+/**
+ *
+ * @author dano2080
+ */
+public class MediaClass {
+    Image image;
+
+    public MediaClass() {
+        //this.image = new Image("src/aquahex.png");
+        //image = new Image(getClass().getResourceAsStream("/src/aquahex.png"));
+         //InputStream is = ImageLoader.class.getResourceAsStream("/src/aquahex.png");
+         //image = new Image(getClass().getClassLoader().getResourceAsStream("src/aquahex.png"));
+        //Image image = ImageIO.read(is);
+               //this.image = new Image("src/aquahex.png");
+
+    }
+
+       //graphicsContext.drawImage(image, 10.0, 10.0);
+       
+       //Image image = new Image(getClass().getResourceAsStream("/src/aquahex.png"));
+        //int[] boardBoundaries = new int[]{0.0,2,3,4};
+        //800x600
+     //final Canvas canvas = new Canvas(250,250);
+//final GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
+ //Image image;
+ 
+    //graphicsContext.drawImage( image, 10.0, 10.0);
+}
