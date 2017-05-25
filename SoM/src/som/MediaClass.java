1ec2e5db5fa8f55aebe215552c444dbf8bfa96c2
dano80 <danielino3@yahoo.com>
2017-05-25 09:11:24 -0700

    add audio media class

--- SoM/src/som/MediaClass.java
+++ SoM/src/som/MediaClass.java
@@ -6,34 +6,67 @@
 package som;
 
 import com.sun.javafx.iio.ImageLoader;
+import java.io.File;
 import java.io.InputStream;
 import javafx.scene.image.Image;
+import javafx.scene.media.AudioClip;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import static som.SoM.mediaPlayer1;
 
 /**
  *
  * @author dano2080
  */
 public class MediaClass {
-    Image image;
+    //Image image;
+    static MediaPlayer mediaPlayer1,mediaPlayer2; 
+    MediaView  mediaView;
 
     public MediaClass() {
-        //this.image = new Image("src/aquahex.png");
-        //image = new Image(getClass().getResourceAsStream("/src/aquahex.png"));
-         //InputStream is = ImageLoader.class.getResourceAsStream("/src/aquahex.png");
-         //image = new Image(getClass().getClassLoader().getResourceAsStream("src/aquahex.png"));
-        //Image image = ImageIO.read(is);
-               //this.image = new Image("src/aquahex.png");
-
+        //start
+        loadAudioAssets(); 
+        
+        playMusic1();
     }
 
-       //graphicsContext.drawImage(image, 10.0, 10.0);
-       
-       //Image image = new Image(getClass().getResourceAsStream("/src/aquahex.png"));
-        //int[] boardBoundaries = new int[]{0.0,2,3,4};
-        //800x600
-     //final Canvas canvas = new Canvas(250,250);
-//final GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
- //Image image;
- 
-    //graphicsContext.drawImage( image, 10.0, 10.0);
+    private void loadAudioAssets(){
+        String music1Path = "src/som/music/WASTELAND2melody.wav";
+    Media mMusic1 = new Media(new File(music1Path).toURI().toString());
+     String music2Path = "src/som/music/EDGEofAWARE1wbrass.wav";
+    Media mMusic2 = new Media(new File(music2Path).toURI().toString());
+        
+        mediaPlayer1 = new MediaPlayer(mMusic1); //mediaPlayer
+        mediaPlayer2 = new MediaPlayer(mMusic2); //mediaPlayer
+        mediaView = new MediaView(mediaPlayer1);
+        mediaView.setMediaPlayer(mediaPlayer1);
+        
+        /*musicURL1 = getClass().getResource("/WASTELAND1.wav");
+        aMusic1 = new AudioClip(musicURL1.toString());
+        musicURL2 = getClass().getResource("/WASTELAND2.wav");
+        aMusic2 = new AudioClip(musicURL2.toString());*/
+    }
+    
+    public static void playMusic1() {
+        //aMusic1.setCycleCount(AudioClip.INDEFINITE);
+        //aMusic1.play();
+        
+       mediaPlayer1.setCycleCount(AudioClip.INDEFINITE);
+        mediaPlayer1.play();
+        //if(mediaPlayer2.getStatus().equals(Status.PLAYING))
+           // mediaPlayer2.stop();
+    }
+    
+    public static void playMusic2() {
+        //aMusic2.setCycleCount(AudioClip.INDEFINITE);
+        //aMusic2.play();
+        
+        mediaPlayer2.setCycleCount(AudioClip.INDEFINITE);
+        //mediaPlayer1.play();
+        
+        mediaPlayer2.play();
+        if(mediaPlayer1.getStatus().equals(MediaPlayer.Status.PLAYING))
+            mediaPlayer1.stop();
+    }
 }
