73d449529bd8214c77c291b3b272f139e96fda91
dano80 <danielino3@yahoo.com>
2017-05-27 23:28:34 -0700

    audio class

--- SoM/src/som/Audio.java
+++ SoM/src/som/Audio.java
@@ -33,153 +33,88 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-
 /**
  *
  * @author dano2080
  */
 public class Audio {
+
     Media media;
     MediaPlayer mediaPlayer;
     String music1Path;
-    
+
     InputStream resource;
-   static AudioClip aMusic1,aMusic2;
-    static MediaPlayer mediaPlayer1,mediaPlayer2; 
-    MediaView  mediaView;
-    static URL musicURL1,musicURL2;
-    
-    
-    
-//    
-//    URL url = getClass().getResource("").toURI().toURL();
-//String applicationDir = url.getPath();
+    static AudioClip aMusic1, aMusic2, soundClips;
+    static MediaPlayer mediaPlayer1, mediaPlayer2;
+    MediaView mediaView;
+    static URL musicURL1, musicURL2, sounds;
 
-//if(url.getProtocol().equals("jar")) {
-//    appDir = new File(((JarURLConnection)url.openConnection()).getJarFileURL().getFile()).().getFile()).getParent();
-//}
+    static ArrayList<String> audioClips = new ArrayList();
 
-//String mp3Dir = appDir + "ProjectName" + System.getProperty("file.separator") + "mp3Directory";
-    
-    //final File dir = new File("music");
-    //private static final String MUSIC_DIR = "/Users/dano2080/Desktop/music";
-    
-    //final ProgressBar progress = new ProgressBar();
-    //private ChangeListener<Duration> progressChangeListener;
+    public Audio() {
 
-    
-
-   
-
-
-
-    public Audio(){
 //    resource = this.getClass().getResourceAsStream("/Team-Project/SoM/src/som/music/WASTELAND1.wav");
-//aMusic1 = new AudioClip(musicURL1.toString());  
-//     musicURL1 = getClass().getResource("/WASTELAND1.wav");
-//        aMusic1 = new AudioClip(musicURL1.toString());
-//        musicURL2 = getClass().getResource("/WASTELAND2.wav");
-//        aMusic2 = new AudioClip(musicURL2.toString());
-    //start
-        loadAudioAssets(); 
-        
+        //start
+        loadAudioAssets();
         playMusic1();
-        //playSound();
-    
     }
-    private void loadAudioAssets(){
-       String music1Path = "src/som/music/WASTELAND2melody.wav";
-    Media mMusic1 = new Media(new File(music1Path).toURI().toString());
-     String music2Path = "src/som/music/EDGEofAWARE1wbrass.wav";
-    Media mMusic2 = new Media(new File(music2Path).toURI().toString());
-        
+
+    public void playClips(int i) {
+        //sounds = getClass().getResource("audio/sounds/building.wav");
+        sounds = getClass().getResource(audioClips.get(i));
+        soundClips = new AudioClip(sounds.toString());
+        soundClips.play();
+    }
+
+    private void loadAudioAssets() {
+        String path = "audio/sounds/";
+        audioClips.add(path + "building.wav");
+        audioClips.add(path + "dice.wav");
+        audioClips.add(path + "drop.wav");
+        audioClips.add(path + "glass.wav");
+        audioClips.add(path + "hemp.wav");
+        audioClips.add(path + "Plastic.wav");
+        audioClips.add(path + "steel.wav");
+        audioClips.add(path + "water.wav");
+
+        String music1Path = "src/som/audio/music/WASTELAND2melody.wav";
+        Media mMusic1 = new Media(new File(music1Path).toURI().toString());
+        String music2Path = "src/som/audio/music/EDGEofAWARE1wbrass.wav";
+        Media mMusic2 = new Media(new File(music2Path).toURI().toString());
+
         mediaPlayer1 = new MediaPlayer(mMusic1); //mediaPlayer
         mediaPlayer2 = new MediaPlayer(mMusic2); //mediaPlayer
         mediaView = new MediaView(mediaPlayer1);
         mediaView.setMediaPlayer(mediaPlayer1);
-        
-        musicURL1 = getClass().getResource("music/WASTELAND1.wav");
+
+        musicURL1 = getClass().getResource("audio/music/WASTELAND1.wav");
         aMusic1 = new AudioClip(musicURL1.toString());
-        musicURL2 = getClass().getResource("music/WASTELAND2.wav");
+        musicURL2 = getClass().getResource("audio/music/WASTELAND2.wav");
         aMusic2 = new AudioClip(musicURL2.toString());
     }
-    public void playSound(){
-         
-        try {
-        media = new Media(music1Path);
-        if (media.getError() == null) {
-            media.setOnError(new Runnable() {
-                public void run() {
-                    // Handle asynchronous error in Media object.
-                }
-            });
-            try {
-                mediaPlayer = new MediaPlayer(media);
-                if (mediaPlayer.getError() == null) {
-                    mediaPlayer.setOnError(new Runnable() {
-                        public void run() {
-                            // Handle asynchronous error in MediaPlayer object.
-                        }
-                    });
-                    mediaView = new MediaView(mediaPlayer);
-                    mediaView.setOnError(new EventHandler() {
-                        public void handle(MediaErrorEvent t) {
-                            // Handle asynchronous error in MediaView.
-                        }
 
-                        @Override
-                        public void handle(Event event) {
-                            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-                        }
-                    });
-                } else {
-                    // Handle synchronous error creating MediaPlayer.
-                }
-            } catch (Exception mediaPlayerException) {
-                // Handle exception in MediaPlayer constructor.
-            }
-        } else {
-            // Handle synchronous error creating Media.
-        }
-    } catch (Exception mediaException) {
-        // Handle exception in Media constructor.
-    }
-        
-    }
     public static void playMusic1() {
-//        aMusic1.setCycleCount(AudioClip.INDEFINITE);
-//        aMusic1.play();
-        
-       mediaPlayer1.setCycleCount(AudioClip.INDEFINITE);
+
+        mediaPlayer1.setCycleCount(AudioClip.INDEFINITE);
         mediaPlayer1.play();
-        if(mediaPlayer2.getStatus().equals(Status.PLAYING))
+        if (mediaPlayer2.getStatus().equals(Status.PLAYING)) {
             mediaPlayer2.stop();
+        }
     }
-    
+
     public static void playMusic2() {
-        //aMusic2.setCycleCount(AudioClip.INDEFINITE);
-        //aMusic2.play();
-        
+
         mediaPlayer2.setCycleCount(AudioClip.INDEFINITE);
-        //mediaPlayer1.play();
-        
         mediaPlayer2.play();
-        if(mediaPlayer1.getStatus().equals(MediaPlayer.Status.PLAYING))
+        if (mediaPlayer1.getStatus().equals(MediaPlayer.Status.PLAYING)) {
             mediaPlayer1.stop();
+        }
     }
 }
-       
-
-
-
-
-
-
 
 // determine the source directory for the playlist (either the first argument to the program or a default).
-        //final List<String> params = request.getParameters().getRaw();
+//final List<String> params = request.getParameters().getRaw();
 //        final File dir =  new File(MUSIC_DIR);
-        
 //
 //        // create some media players.
 //        final List<MediaPlayer> players = new ArrayList<>();
